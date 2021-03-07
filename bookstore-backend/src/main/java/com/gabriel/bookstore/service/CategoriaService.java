daa5e4cdd4d712d6a68b227d2793682ea744a143
gabrielppd77 <gabrielppd77@outlook.com>
2021-03-07 17:46:09 -0300

    First commit again

---
+++ bookstore-backend/src/main/java/com/gabriel/bookstore/service/CategoriaService.java
@@ -0,0 +1,51 @@
+package com.gabriel.bookstore.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.gabriel.bookstore.domain.Categoria;
+import com.gabriel.bookstore.dtos.CategoriaDTO;
+import com.gabriel.bookstore.exceptions.ObjectNotFoundException;
+import com.gabriel.bookstore.repositories.CategoriaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaService {
+    
+    @Autowired
+    private CategoriaRepository repository;
+
+    public Categoria findById(Integer id){
+        Optional<Categoria> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: " + id + ", Tipo: " + Categoria.class.getName()));
+    }
+
+    public List<Categoria> findAll(){
+        return repository.findAll();
+    }
+
+    public Categoria create(Categoria obj){
+        obj.setId(null);
+        return repository.save(obj);
+    }
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+        obj.setNome(objDto.getNome());
+        obj.setDescricao(objDto.getDescricao());
+        return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+        findById(id);
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new com.gabriel.bookstore.exceptions.DataIntegrityViolationException("Categoria não pode ser deletada! Possuí livros associados");
+        }
+        
+	}
+}
---
+++ bookstore-frontend/src/app/components/views/livro/livro-read/livro-read.component.ts
@@ -0,0 +1,39 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { LivroService } from '../livro.service';
+import { Livro } from '../livro.model';
+@Component({
+  selector: 'app-livro-read',
+  templateUrl: './livro-read.component.html',
+  styleUrls: ['./livro-read.component.css']
+})
+export class LivroReadComponent implements OnInit {
+
+  id_cat: String = '';
+
+  livro: Livro = {
+    id: '',
+    titulo: '',
+    nome_autor: '',
+    texto: ''
+  }
+
+  constructor(private service: LivroService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    this.id_cat = this.route.snapshot.paramMap.get('id_cat')!;
+    this.livro.id = this.route.snapshot.paramMap.get('id')!;
+    this.findById();
+  }
+
+  return(): void {
+    this.router.navigate([`categorias/${this.id_cat}/livros`]);
+  }
+
+  findById(): void{
+    this.service.findById(this.livro.id!).subscribe((resposta) => {
+      this.livro = resposta
+    });
+  }
+
+}
